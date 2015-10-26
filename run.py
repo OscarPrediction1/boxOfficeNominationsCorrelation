75218b310a19d44b91da8946960ad4dc9840c947
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-10-26 10:31:03 +0100

    Taking the release date of the movie as a denominator into account

--- run.py
+++ run.py
@@ -4,31 +4,42 @@ import db
 client = MongoClient(db.conn_string)
 db = client.oscar
 
+results = []
+
 # find all nominees
 for data in db.oscar_nominations.find():
-
-	# process a single year
-	print "\n"
-	print "Jahr", data["_id"]
-	print ""
 
 	# loop cateogories
 	for key in data:
 
 		if key == "BEST PICTURE":
 
-			print "\t", key
-
 			# loop nominees
 			for nominee in data[key]:
 
-				line = "\t\t" + nominee["name"]
-
-				if(nominee["won"]):
-					line += u" \U0001F3C6"
+				item = [str(data["_id"]), key, nominee["name"], str(nominee["won"])]
+				
 
 				boxOfficeData = db.boxoffice_movies.find_one({"name": nominee["name"]})
 				if boxOfficeData:
-					line += " $" + "{0:,}".format(boxOfficeData["totalGross"]).replace(",",".")
 
-				print line
+					# calculate days till the end of the year
+					approxDaysItRan = 365 - boxOfficeData["release"].timetuple().tm_yday
+
+					item.append(str(boxOfficeData["totalGross"]))
+					item.append(str(approxDaysItRan))
+					item.append(str(boxOfficeData["release"]))
+				else:
+					item.append("")
+					item.append("")
+					item.append("")
+
+				results.append(item)
+
+for row in results:
+	line = ""
+
+	for cell in row:
+		line += cell + ";"
+
+	print line
