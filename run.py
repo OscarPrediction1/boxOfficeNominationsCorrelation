dac80209e542f3c5abe6a913b0d14491245eb82d
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-11-12 14:47:25 +0100

    Neue Ergebnisse

--- run.py
+++ run.py
@@ -19,31 +19,33 @@ for data in db.oscar_nominations.find():
 
 				item = [str(data["_id"]), key, nominee["name"].replace(",", ""), str(nominee["won"])]
 				
-
 				boxOfficeData = db.boxoffice_movies.find_one({"name": nominee["name"]})
 				if boxOfficeData:
 
 					# calculate days till the end of the year
-					approxDaysItRan = 365 - boxOfficeData["release"].timetuple().tm_yday
+					#approxDaysItRan = 365 - boxOfficeData["release"].timetuple().tm_yday
 
 					lastDay = None
 
 					# find gross at the end of the year
 					if "history" in boxOfficeData:
-						for day in boxOfficeData["history"]:
-							lastDay = day
 
-							if day["date"].day == 31 and day["date"].month == 12 and day["date"].year == boxOfficeData["release"].year:
-								break
+						#for day in boxOfficeData["history"]:
+						#	lastDay = day
+						#
+						#	if day["date"].day == 31 and day["date"].month == 12 and day["date"].year == boxOfficeData["release"].year:
+						#		break
+						lastDay = boxOfficeData["history"][-1]
 
 					if lastDay:
 						item.append(str(lastDay["grossToDate"]))
-						item.append(str(int(lastDay["grossToDate"] / approxDaysItRan)))
+						item.append(str(int(lastDay["grossToDate"] / int(lastDay["dayNumber"]))))
+						item.append(str(lastDay["dayNumber"]))
 					else:
 						item.append("")
 						item.append("")
-
-					item.append(str(approxDaysItRan))
+						item.append("")
+					
 					item.append(str(boxOfficeData["release"]))
 				else:
 					item.append("")
